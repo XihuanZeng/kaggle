28fcd1018b5aa2d50356c12757fd0c899085997b
Xihuan Zeng <zengx272@umn.edu>
2016-09-18 21:54:34 -0500

    Delete train_libffm.sh

--- expedia/train_libffm.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-cd ./models
-for ((i=0;i<=99;i++))
-do
-	python ../code/preprocess_libffmgen.py --cluster ${i}
-	../libffm/ffm-train -t 20 ../data/libffm_data/train.txt libffm_model${i}
-done
-
